45727af8d2120ea85ae255b9e87007ccfb8ff70a
gomdori-foobar <gomdori.foobar@gmail.com>
2025-01-30 16:11:53 +0900

    feature: move factory to root

---
+++ factory.go
@@ -0,0 +1,9 @@
+package bearden
+
+import (
+	"github.com/gomdori-foo/bearden/internal/core/module"
+)
+
+func Create(constructor func() *module.ModuleFactory) *BearDenApplication {
+	return &BearDenApplication{}
+}
